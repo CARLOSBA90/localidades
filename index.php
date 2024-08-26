588d6ccf43a43df9e5ed112ff5ecb243872ff657
Carlos Peña <cepr0190@gmail.com>
2024-08-25 22:11:04 -0300

    --V1.1

--- index.php
+++ index.php
@@ -18,17 +18,14 @@
 
 <script>
   (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "AIzaSyAp9N0ussHMpomEZTECdwpx0j38RKXIoK0",
+    key: "",
     v: "weekly",
-    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-    // Add other bootstrap parameters as needed, using camel case.
   });
 </script>
- 
 </head>
+
 <body class="bg-gray-100">
     <div class="h-screen flex flex-col">
-
         <div class="h-1/5 bg-gray-100 flex items-center justify-center m-auto w-full p-4">
            
         <div class="w-full bg-white p-6 rounded-lg shadow-lg mt-4">
@@ -38,7 +35,7 @@
                 <button class="bg-gray-600 w-40 text-white p-1 rounded-md hover:bg-gray-700" id="buscarBtn">Buscar</button>
             </div>
 
-                    <div class="h-20 overflow-y-auto border border-gray-300 rounded-md">
+                <div class="h-20 overflow-y-auto border border-gray-300 rounded-md">
                         <ul id="resultados"></ul>
                 </div>
         </div>
@@ -52,9 +49,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.js"></script>
     <script src="scripts.js"></script>
-   
-<script>
-</script>
 
 </body>
 </html>
